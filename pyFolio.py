61eb49180b7c937b9e0f67445f47312d9423e9aa
Jordan M. Young <54070169+Jordan-M-Young@users.noreply.github.com>
2020-07-27 20:41:08 -0400

    Update pyFolio.py

--- pyFolio.py
+++ pyFolio.py
@@ -4,7 +4,7 @@ from PyQt5 import QtCore
 from PyQt5 import QtWidgets as qtw
 
 
-import pyFolio_tools as pft
+import pmaso_tools as pft
 
 class MainWindow(qtw.QMainWindow):
     """Main Window of the pyFolio GUI"""
